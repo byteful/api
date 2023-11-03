a4191178e922518adda116d616c597628cb04375
byteful <>
2023-11-03 18:04:17 +0000

    Changes

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const path = require('path')
 const cached = {
   NightMarket: null,
   PictureAds: null,
-  LevelTools: null
+  LevelTools: null,
+  LevelItems: null
 };
 
 cache().then(() => console.log("Successfully loaded cache.")).catch((e) => {
@@ -17,20 +18,24 @@ cache().then(() => console.log("Successfully loaded cache.")).catch((e) => {
 function cache() {
   return new Promise(async (resolve, reject) => {
     try {
-      let res = await fetch('https://api.spigotmc.org/legacy/update.php?resource=105386');
+      let res = await fetch('https://api.spigotmc.org/legacy/update.php?resource=105386', {cache: "no-cache"});
       cached.PictureAds = await res.text();
 
-      //res = await fetch('https://api.spigotmc.org/legacy/update.php?resource=105789');
-      cached.NightMarket = fs.readFileSync("NightMarket.txt", { encoding: "UTF-8" }); // await res.text();
+      res = await fetch('https://api.spigotmc.org/legacy/update.php?resource=105789', {cache: "no-cache"});
+      //cached.NightMarket = fs.readFileSync("NightMarket.txt", { encoding: "UTF-8" });
+      cached.NightMarket = await res.text();
 
-      res = await fetch('https://api.spigotmc.org/legacy/update.php?resource=97516');
+      res = await fetch('https://api.spigotmc.org/legacy/update.php?resource=97516', {cache: "no-cache"});
       cached.LevelTools = await res.text();
+
+      cached.LevelItems = fs.readFileSync("LevelItems.txt", { encoding: "UTF-8" });
 
       console.log(cached);
 
       fs.writeFileSync("PictureAds.txt", cached.PictureAds, { encoding: "UTF-8" })
       fs.writeFileSync("NightMarket.txt", cached.NightMarket, { encoding: "UTF-8" })
       fs.writeFileSync("LevelTools.txt", cached.LevelTools, { encoding: "UTF-8" })
+      fs.writeFileSync("LevelItems.txt", cached.LevelItems, { encoding: "UTF-8" })
       resolve()
     } catch (e) {
       reject(e)
@@ -42,6 +47,7 @@ function localCache() {
   cached.PictureAds = fs.readFileSync("PictureAds.txt", { encoding: "UTF-8" });
   cached.LevelTools = fs.readFileSync("LevelTools.txt", { encoding: "UTF-8" });
   cached.NightMarket = fs.readFileSync("NightMarket.txt", { encoding: "UTF-8" });
+  cached.LevelItems = fs.readFileSync("LevelItems.txt", { encoding: "UTF-8" });
   console.log("Loaded local-cache because SpigotMC API failed.")
 }
 
@@ -49,17 +55,27 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "info.html"))
 });
 
+// "logging" IPs so i can see if anybody abusing the API, not gonna add ratelimit or such rn cause honestly idc its replit.com's servers but if it starts lagging stuff out i'll add ratelimit per IP
+
 app.get('/nightmarket', (req, res) => {
   res.send(cached.NightMarket);
+  console.log("API Hit for NightMarket: " + (req.headers['x-forwarded-for'] || req.socket.remoteAddress));
 });
 
 app.get('/pictureads', (req, res) => {
   res.send(cached.PictureAds);
+  console.log("API Hit for PictureAds: " + (req.headers['x-forwarded-for'] || req.socket.remoteAddress));
 });
 
 app.get('/leveltools', (req, res) => {
   res.send(cached.LevelTools);
+  console.log("API Hit for LevelTools: " + (req.headers['x-forwarded-for'] || req.socket.remoteAddress));
 });
+
+app.get("/levelitems", (req, res) => {
+  res.send(cached.LevelItems);
+  console.log("API Hit for LevelItems: " + (req.headers['x-forwarded-for'] || req.socket.remoteAddress));
+})
 
 app.listen(3000, () => {
   console.log('server started');
